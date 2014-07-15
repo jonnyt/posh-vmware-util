02e7907c20b78195245f8dfa11d4eda4887edc61
jonnyt <jonathont@gmail.com>
2014-07-15 11:47:40 -0700

    Additional storage methods
    Get-DatastoreMountInfo, Attach-Datastore, Mount-Datastore

--- VMware.psm1
+++ VMware.psm1
@@ -461,14 +461,125 @@ Author: Jonathon Taylor
 	}
 }
 
+Function Get-DatastoreMountInfo
+{
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline=$true)]
+		$Datastore
+	)
+	Process {
+		$AllInfo = @()
+		if (-not $Datastore) {
+			$Datastore = Get-Datastore
+		}
+		Foreach ($ds in $Datastore) {  
+			if ($ds.ExtensionData.info.Vmfs) {
+				$hostviewDSDiskName = $ds.ExtensionData.Info.vmfs.extent[0].diskname
+				if ($ds.ExtensionData.Host) {
+					$attachedHosts = $ds.ExtensionData.Host
+					Foreach ($VMHost in $attachedHosts) {
+						$hostview = Get-View $VMHost.Key
+						$hostviewDSState = $VMHost.MountInfo.Mounted
+						$StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+						$devices = $StorageSys.StorageDeviceInfo.ScsiLun
+						Foreach ($device in $devices) {
+							$Info = "" | Select Datastore, VMHost, Lun, Mounted, State
+							if ($device.canonicalName -eq $hostviewDSDiskName) {
+								$hostviewDSAttachState = ""
+								if ($device.operationalState[0] -eq "ok") {
+									$hostviewDSAttachState = "Attached"							
+								} elseif ($device.operationalState[0] -eq "off") {
+									$hostviewDSAttachState = "Detached"							
+								} else {
+									$hostviewDSAttachState = $device.operationalstate[0]
+								}
+								$Info.Datastore = $ds.Name
+								$Info.Lun = $hostviewDSDiskName
+								$Info.VMHost = $hostview.Name
+								$Info.Mounted = $HostViewDSState
+								$Info.State = $hostviewDSAttachState
+								$AllInfo += $Info
+							}
+						}
+						
+					}
+				}
+			}
+		}
+		$AllInfo
+	}
+}
 
-Export-ModuleMember -Function Connect-VI
-Export-ModuleMember -Function Get-ViSession
-Export-ModuleMember -Function Disconnect-ViSession
-Export-ModuleMember -Function Set-SIOC
-Export-ModuleMember -Function Remove-vLicense
+Function Mount-Datastore {
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline=$true)]
+		$Datastore
+	)
+	Process {
+		if (-not $Datastore) {
+			Write-Host "No Datastore defined as input"
+			Exit
+		}
+		Foreach ($ds in $Datastore) {
+			$hostviewDSDiskName = $ds.ExtensionData.Info.vmfs.extent[0].Diskname
+			if ($ds.ExtensionData.Host) {
+				$attachedHosts = $ds.ExtensionData.Host
+				Foreach ($VMHost in $attachedHosts) {
+					$hostview = Get-View $VMHost.Key
+					$StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+					Write-Host "Mounting VMFS Datastore $($DS.Name) on host $($hostview.Name)..."
+					$StorageSys.MountVmfsVolume($DS.ExtensionData.Info.vmfs.uuid);
+				}
+			}
+		}
+	}
+}
+
+Function Attach-Datastore {
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline=$true)]
+		$Datastore
+	)
+	Process {
+		if (-not $Datastore) {
+			Write-Host "No Datastore defined as input"
+			Exit
+		}
+		Foreach ($ds in $Datastore) {
+			$hostviewDSDiskName = $ds.ExtensionData.Info.vmfs.extent[0].Diskname
+			if ($ds.ExtensionData.Host) {
+				$attachedHosts = $ds.ExtensionData.Host
+				Foreach ($VMHost in $attachedHosts) {
+					$hostview = Get-View $VMHost.Key
+					$StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+					$devices = $StorageSys.StorageDeviceInfo.ScsiLun
+					Foreach ($device in $devices) {
+						if ($device.canonicalName -eq $hostviewDSDiskName) {
+							$LunUUID = $Device.Uuid
+							Write-Host "Attaching LUN $($Device.CanonicalName) to host $($hostview.Name)..."
+							$StorageSys.AttachScsiLun($LunUUID);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
 Export-ModuleMember -Function Add-vLicense
+Export-ModuleMember -Function Attach-Datastore
+Export-ModuleMember -Function Connect-VI
+Export-ModuleMember -Function Detach-Datastore
+Export-ModuleMember -Function Disconnect-ViSession
+Export-ModuleMember -Function Get-DatastoreMountInfo
+Export-ModuleMember -Function Get-ViSession
 Export-ModuleMember -Function Get-vLicense
 Export-ModuleMember -Function Get-VMFolderStruct
+Export-ModuleMember -Function Mount-Datastore
+Export-ModuleMember -Function Remove-vLicense
+Export-ModuleMember -Function Set-SIOC
 Export-ModuleMember -Function Unmount-Datastore
-Export-ModuleMember -Function Detach-Datastore 
